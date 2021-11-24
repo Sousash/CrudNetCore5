dd90694bea579cb5c5be58b247701ae68b7db77d
Gabriel López <gabriellquejigo@gmail.com>
2021-11-24 11:27:51 +0100

    Agregar archivos de proyecto.

---
+++ CrudNetCore5/Controllers/BooksController.cs
@@ -0,0 +1,124 @@
+﻿using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CrudNetCore5.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IEnumerable<Book> listBooks = _context.Book;
+
+            return View(listBooks);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book b)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(b);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Book has been created succesfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //get the book
+            var book = _context.Book.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Book b)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(b);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Book has been updated succesfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //get the book
+            var book = _context.Book.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            // get the book
+            var b = _context.Book.Find(id);
+
+            if (b == null)
+            {
+                return NotFound();
+            }
+
+            
+            _context.Remove(b);
+            _context.SaveChanges();
+
+            TempData["mensaje"] = "Book has been deleted succesfully";
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ CrudNetCore5/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudNetCore5.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; } 
+
+        [Required(ErrorMessage = "Title is a must")]
+        [StringLength(50, ErrorMessage = "{0} lenght must be at least {1}", MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is a must")]
+        [StringLength(100, ErrorMessage = "{0} lenght must be at least {1}")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Release Date is a must")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public System.DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Author is a must")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Price is a must")]
+        public int Price { get; set; }
+
+    }
+}
